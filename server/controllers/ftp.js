22f5adf561e96931bc2135844228b101acb48c51
Kamrul Islam~ <kamrulbeximco@gmail.com>
2022-09-25 11:57:51 +0600

    changed

--- server/controllers/ftp.js
+++ server/controllers/ftp.js
@@ -79,51 +79,42 @@ const Papa = require('papaparse')
 
 
 
-const PromiseFtp = require('promise-ftp'),
-    ftp = new PromiseFtp()
+const PromiseFtp = require('promise-ftp'), ftp = new PromiseFtp()
 const csv = require("csvtojson")
 
-var csvFile = {
-    formatA: ['/I_EVTSCAN.csv'],
-    formatB: ['/I_SHIFTEVT.CSV'],
-    formatC: ['/I_STATUS.csv']
-}
 
-// var csvFiles = ['/I_EVTSCAN.csv', '/I_SHIFTEVT.CSV', '/I_STATUS.csv']
-// var csvFiles = ['/I_EVTSCAN.csv', '/I_STATUS.csv']
-// var csvFiles = ['/I_STATUS.csv', 'I_DOFFPRD.CSV']
 var csvFiles = [
     {
-        name: 'I_STATUS',
-        path: '/I_STATUS.csv'
+        file_name: 'I_STATUS',
+        file_path: '/I_STATUS.csv'
     },
     {
-        name: 'I_DOFFPRD',
-        path: 'I_DOFFPRD.CSV',
+        file_name: 'I_DOFFPRD',
+        file_path: '/I_DOFFPRD.CSV',
     },
     {
-        name: 'I_EVTSCAN',
-        path: '/I_EVTSCAN.csv',
+        file_name: 'I_EVTSCAN',
+        file_path: '/I_EVTSCAN.csv',
     }
 ]
-// var csvFiles = ['I_DOFFPRD.CSV']
 
 const serverIP = [
-    { ip: '192.168.114.23', name: 'machine1' },
-    { ip: '192.168.114.23', name: 'machine2' }
+    { ip: '192.168.114.24', machine_name: 'machine1' },
+    { ip: '192.168.114.24', machine_name: 'machine2' }
 ]
 
 var jsonArray = []
 var jsonObject = {}
+
 exports.READ_CSV_FILE = async (req, res) => {
     try {
         for (const server of serverIP) {
             for (const file of csvFiles) {
-                await ftpSingleFileRead(file.path, file.name, server.ip, server.name)
+                await ftpSingleFileRead(file.file_path, file.file_name, server.ip, server.machine_name)
             }
         }
         // for (const file of csvFiles) {
-        //     await fptUpdate(file.path, file.name)
+        //     await fptUpdate(file.file_path, file.name)
         //     // console.log(jsonArray.length)
         //     // jsonArray = []
         //     // console.log('Processed file: ' + file)
@@ -135,44 +126,14 @@ exports.READ_CSV_FILE = async (req, res) => {
 }
 
 var credentails = {
-    host: "192.168.114.23",
+    host: "192.168.114.24",
     user: "beximco",
-    password: "beximco"
-}
-const fptUpdate = async (path, name) => {
-    await ftp.connect(credentails).then(function () {
-        return ftp.get(path)
-    }).then(function (stream) {
-        return new Promise(function (resolve, reject) {
-            var csvData = []
-            stream.pipe(csv({ noheader: true }))
-                .on('data', (row) => { // Calls for every row fetching
-                    /** Data calls for every row of csv file(As it is coming by stream concept)
-                     * So we have to make an array from all rows
-                     * convert row to string and string to json
-                     * Then push it to an array(Here csvData)
-                     */
-                    const jsonFormatedRow = JSON.parse(row.toString('utf8')) // stream row -> string -> json
-                    csvData.push(jsonFormatedRow) // Making array
-                })
-                .on('end', () => { // Calls after finishing all rows fetch
-                    jsonArray.push({
-                        file_path: path,
-                        file_name: name,
-                        [name]: csvData
-                    })
-                    resolve()
-                })
-            stream.on('error', (err) => reject(err))
-        })
-    }).then(function () {
-        return ftp.end()
-    })
+    password: "123456"
 }
 
-const ftpSingleFileRead = async (path, name, server_ip, server_name) => {
+const ftpSingleFileRead = async (file_path, file_name, server_ip, server_name) => {
     await ftp.connect({ ...credentails, host: server_ip }).then(function () {
-        return ftp.get(path)
+        return ftp.get(file_path)
     }).then(function (stream) {
         return new Promise(function (resolve, reject) {
             var csvData = []
@@ -190,10 +151,13 @@ const ftpSingleFileRead = async (path, name, server_ip, server_name) => {
                     jsonObject = {
                         ...jsonObject,
                         [server_name]: {
+                            server_ip,
                             ...jsonObject[server_name],
-                            file_path: path,
-                            file_name: name,
-                            [name]: csvData
+                            [file_name]: {
+                                file_path: file_path,
+                                file_name: file_name,
+                                data: csvData
+                            }
                         }
                     }
                     resolve()
@@ -204,3 +168,36 @@ const ftpSingleFileRead = async (path, name, server_ip, server_name) => {
         return ftp.end()
     })
 }
+
+
+const fptUpdate = async (file_path, name) => {
+    await ftp.connect(credentails).then(function () {
+        return ftp.get(file_path)
+    }).then(function (stream) {
+        return new Promise(function (resolve, reject) {
+            var csvData = []
+            stream.pipe(csv({ noheader: true }))
+                .on('data', (row) => { // Calls for every row fetching
+                    /** Data calls for every row of csv file(As it is coming by stream concept)
+                     * So we have to make an array from all rows
+                     * convert row to string and string to json
+                     * Then push it to an array(Here csvData)
+                     */
+                    const jsonFormatedRow = JSON.parse(row.toString('utf8')) // stream row -> string -> json
+                    csvData.push(jsonFormatedRow) // Making array
+                })
+                .on('end', () => { // Calls after finishing all rows fetch
+                    jsonArray.push({
+                        file_path: file_path,
+                        file_name: name,
+                        [name]: csvData
+                    })
+                    resolve()
+                })
+            stream.on('error', (err) => reject(err))
+        })
+    }).then(function () {
+        return ftp.end()
+    })
+}
+
